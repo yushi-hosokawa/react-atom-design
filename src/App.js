748cfa0b983a29e3f37757c7e12a76cd10744beb
yushi hosokawa <49804320+yushi-hosokawa@users.noreply.github.com>
2021-05-24 01:13:26 +0900

    organisｍの作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  image: "https://source.unsplash.com/IuJc2qh2TcA",
+  name: "たけし",
+  email: "12345example.com",
+  phone: "00-00-0000",
+  company: {
+    name: "てすと株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -9,6 +21,7 @@ export default function App() {
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
